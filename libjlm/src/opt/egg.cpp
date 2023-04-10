945e42b7543b680343325ccdf4c0eaf7eabd00e5
Emirhan Demir <ebdemir@genet.com.tr>
2023-04-10 22:28:22 +0200

    Add write to temp file

--- libjlm/src/opt/egg.cpp
+++ libjlm/src/opt/egg.cpp
@@ -1,13 +1,15 @@
 #include "libjlm/include/jlm/opt/egg.hpp"
 #include "jlm/ir/RvsdgModule.hpp"
 
+#include <cstdio>
 #include <jive/view.hpp>
 
 namespace jlm {
 
 	auto egg::run(RvsdgModule& module, StatisticsCollector& stats) -> void
 	{
-		jive::view(module.Rvsdg().root(), stdout);
-		jive::view_xml(module.Rvsdg().root(), stdout);
+		auto f { std::fopen("tmp.rvsdg", "w") };
+		jive::view(module.Rvsdg().root(), f);
+		// jive::view_xml(module.Rvsdg().root(), f);
 	}
 }
